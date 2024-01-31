78291ef60127fbd56d2616082c9e131e4cb70777
roshani2022 <rosh.gupta17@gmail.com>
2024-01-31 21:21:22 +0530

    Prerendering the powers of Next JS

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,4 +1,3 @@
-
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
@@ -10,5 +9,48 @@ function MeetupDetails() {
       description="this is the best place"
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback:true,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+      
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId)
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2018/05/20/684461-urban-city.jpg",
+        id: meetupId,
+        title: "the first meetup",
+        address: "Nagpur",
+        description: "this is the best place",
+      },
+    },
+  };
 }
 export default MeetupDetails;
