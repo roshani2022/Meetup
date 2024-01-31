c2e8e160f498fc39b814c31d85af4f698bc061fb
roshani2022 <rosh.gupta17@gmail.com>
2024-01-31 20:14:45 +0530

    get static props

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,33 @@
-import MeetupList from '../components/meetups/MeetupList';
+import MeetupList from "../components/meetups/MeetupList";
 
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A first meet up",
+    image:
+      "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2018/05/20/684461-urban-city.jpg",
+    address: "some address1",
+    description: "this is our first meet up",
+  },
+  {
+    id: "m2",
+    title: "A second meet up",
+    image:
+      "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2017/12/shutterstock_7024083491.jpg",
+    address: "nagpur",
+    description: "this is our second meet up",
+  },
+];
 
-function HomePage () {
-
-    const DUMMY_MEETUPS = [
-        {
-            id:"m1",
-            title:"A first meet up",
-            image:"https://cdn.dnaindia.com/sites/default/files/styles/full/public/2018/05/20/684461-urban-city.jpg",
-            address:"some address1",
-            description:"this is our first meet up",
-        },
-        {
-            id:"m2",
-            title:"A second meet up",
-            image:"https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2017/12/shutterstock_7024083491.jpg",
-            address:"nagpur",
-            description:"this is our second meet up",
-        }
-    ]
- return(
-   
-       <MeetupList meetups={DUMMY_MEETUPS}/>
-    
-    
- )
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
 }
-
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate:10
+  };
+}
 export default HomePage;
